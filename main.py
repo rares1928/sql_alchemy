932acdb56a2da829a9ccf882493db485f6ed510a
Rares <rares1928@gmail.com>
2023-06-02 23:27:55 +0300

    first try DB connect

--- main.py
+++ main.py
@@ -1,16 +1,39 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
+import sqlalchemy
+from sqlalchemy import  create_engine
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from sqlalchemy import select
 
+def get_connection():
+    server = os.environ.get("SERVER")
+    user = os.environ.get("username");
+    pwd = os.environ.get("password");
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    url = "mssql+pyodbc://{0}:{1}@{2}:{3}/{4}?driver=ODBC+Driver+17+for+SQL+Server".format(user, pwd, server, 1433, "ouizdb")
+    print(url)
+    return create_engine(url);
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+
+    try:
+        engine = get_connection()
+        connection = engine.connect()
+        print("viata e frumoasa")
+        meta_data = sqlalchemy.MetaData()
+        tabel = sqlalchemy.Table('Questions', meta_data, autoload_with=engine)
+        stmt = sqlalchemy.select([tabel]).limit(10)
+        result = connection.execute(stmt)
+        print(result.fetchall())
+
+
+    except Exception as ex:
+        print("viata nu e frumoasa")
+        print(ex)
+
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
